bd11fc9fcf50b631c8bacd634d0a350a2a86555d
Benjamin Mende <bmende@bowdoin.edu>
2013-07-17 21:05:56 -0400

    using classes better

---
+++ src/BendeChess.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class BendeChess {
+
+    public static void main(String[] args) {
+
+	uciCom thing = new uciCom();
+
+	System.out.println("Thanks for testing BendeChess");
+    }
+}
--- src/uciCom.java
+++ src/uciCom.java
@@ -3,18 +3,26 @@ package chess;
 import java.util.Scanner;
 
 public class uciCom {
+    //the main job of uciCom is reading
+    Scanner in;
 
-    public static void main(String[] args) {
-	Scanner in = new Scanner(System.in);
+    //during construction of communications,
+    //we will perform the initial uci chatter
+    public uciCom() {
+
+	in = new Scanner(System.in);
+
 	if (in.nextLine().equals("uci")) {
 	    System.out.println("id name BendeChess");
 	    System.out.println("id author Benjamin M.");
 	    System.out.println("uciok");
 	}
-
-	while (!in.nextLine().equals("quit"));
+	else {
+	    System.out.println("we want only uci");
+	    System.exit(-1); //we want uci only
+	}
+	
     }
-
 }
 
 	    
